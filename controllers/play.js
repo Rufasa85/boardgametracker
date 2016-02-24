361a987771de79c074bcd47d6d0c99ea309b8837
Joe Rehfuss <joe.rehfuss@gmail.com>
2016-02-24 13:17:11 -0800

    upadated new game form and properly associated players

--- controllers/play.js
+++ controllers/play.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Play = require('../models/play');
+var Player = require('../models/player');
 
 router.get('/', function(req, res) {
     Play.find({}, function(err, plays) {
@@ -12,15 +13,35 @@ router.get('/', function(req, res) {
 
 router.route('/new')
     .get(function(req, res) {
-        res.render('plays/new')
+        Player.find({}, function(err, players) {
+            if (err) res.send(err);
+            res.render('plays/new', {players:players})
+        });
 }).post(function(req, res) {
     console.log(req.body);
-    var playerArray = req.body.players.split(', ');
+    var playerArray = [];
+    if (req.body.player1 !== 'false') {
+        playerArray.push(req.body.player1);
+    };
+    if (req.body.player2 !== 'false') {
+        playerArray.push(req.body.player2);
+    };
+    if (req.body.player3 !== 'false') {
+        playerArray.push(req.body.player3);
+    };
+    if (req.body.player4 !== 'false') {
+        playerArray.push(req.body.player4);
+    };
+    if (req.body.player5 !== 'false') {
+        playerArray.push(req.body.player5);
+    };
+    console.log(playerArray);
     var newPlay = Play({
         game:req.body.game,
         players:playerArray,
         winner: req.body.winner
     });
+    // res.send(newPlay);
     newPlay.save(function(err) {
         if (err) console.log(err);
     });
